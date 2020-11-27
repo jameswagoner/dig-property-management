7de30e60b03a6fd4fc20cb65bde7be257ce0b707
James Wagoner <jameswagoner@gmail.com>
2020-11-26 19:28:30 -0800

    Livewire paginate

--- app/Http/Livewire/Tasks/ListTasks.php
+++ app/Http/Livewire/Tasks/ListTasks.php
@@ -4,9 +4,12 @@ namespace App\Http\Livewire\Tasks;
 
 use App\Models\Task;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ListTasks extends Component
 {
+    use WithPagination;
+
     public $listeners = [
         'task-saved' => 'render'
     ];
@@ -14,7 +17,7 @@ class ListTasks extends Component
     public function render()
     {
         return view('livewire.tasks.list', [
-            'tasks' => Task::orderByDesc('created_at')->get()
+            'tasks' => Task::orderByDesc('created_at')->paginate(5)
         ]);
     }
 
--- resources/views/livewire/tasks/list.blade.php
+++ resources/views/livewire/tasks/list.blade.php
@@ -1,4 +1,5 @@
-<div class="bg-white lg:min-w-0 lg:flex-1">
+<div class="space-y-4">
+    <div class="bg-white shadow-xl sm:rounded-lg">
         <ul class="relative z-0 divide-y divide-gray-200 border-b border-gray-200">
         @foreach($tasks as $task)
             <li class="relative pl-4 pr-6 py-5 hover:bg-gray-50 sm:py-6 sm:pl-6 lg:pl-8 xl:pl-6">
@@ -42,7 +43,11 @@
                         </p>
                     </div>
                 </div>
-        </li>
+            </li>
         @endforeach
         </ul>
     </div>
+    <div>
+        {{ $tasks->links() }}
+    </div>
+</div>
