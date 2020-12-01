2efa28d8d7f1ebfc3985f82474f0248a6a3209c9
James Wagoner <jameswagoner@gmail.com>
2020-11-30 23:14:47 -0800

    Searchable trait

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Property extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $guarded = [];
 
     public function tasks()
     {
         return $this->hasMany(Task::class);
+    }
+
+    public function toSearchableArray()
+    {
+        return $this->toArray();
     }
 }
