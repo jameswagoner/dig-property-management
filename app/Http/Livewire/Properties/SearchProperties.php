ab1c07599273ec65b82a4f82ede3f636f7381912
James Wagoner <jameswagoner@gmail.com>
2020-12-02 22:39:05 -0800

    Property search component

--- app/Http/Livewire/Properties/SearchProperties.php
+++ app/Http/Livewire/Properties/SearchProperties.php
@@ -7,18 +7,48 @@ use Livewire\Component;
 
 class SearchProperties extends Component
 {
-    public $class = '';
+    public $open = false;
+    public $class;
+    public $label;
+    public $query;
+    public $properties;
+    public $property;
 
-    public $label = '';
+    public function mount()
+    {
+        $this->clear();
+    }
 
-    public $search;
+    public function clear()
+    {
+        $this->open = false;
+        $this->query = null;
+        $this->property = null;
+        $this->properties = collect();
 
-    public $queryString = ['search'];
+        $this->emitUp('propertySelected', null);
+    }
+
+    public function updatedQuery()
+    {
+        $this->open = true;
+
+        $this->properties = Property::search($this->query)->get();
+    }
+
+    public function selectProperty(Property $property)
+    {
+        $this->property = $property;
+        $this->query = $property->nickname;
+
+        $this->open = false;
+        $this->properties = collect();
+
+        $this->emitUp('propertySelected', $property->id);
+    }
 
     public function render()
     {
-        return view('livewire.properties.search-properties', [
-            'properties' => Property::search($this->search)->get()
-        ]);
+        return view('livewire.properties.search-properties');
     }
 }
--- resources/views/livewire/properties/search-properties.blade.php
+++ resources/views/livewire/properties/search-properties.blade.php
@@ -1,4 +1,4 @@
-<div class="{{ $class }}">
+<div x-data="{ open: $wire.entangle('open'), query: $wire.entangle('query') }" class="{{ $class }}">
     @if($label)
     <label for="properties_search" class="block text-sm font-medium leading-5 text-gray-900">
         {{ $label }}
@@ -6,21 +6,19 @@
     @endif
     <div class="relative">
         <div class="relative rounded-md shadow-sm">
-            <input id="properties_search" wire:model="search" class="form-input text-sm block w-full" autocomplete="off" />
-            <span class="absolute inset-y-0 right-0 flex items-center pr-4">
-                <button wire:click="reset" type="button" class="p-1 focus:outline-none focus:shadow-outline text-xl text-gray-500"> &times; </button>
+            <input wire:model.debounce.500ms="query" wire:keydown.escape="clear" id="properties_search" class="form-input text-sm block w-full" autocomplete="off" />
+            <span x-show="query" class="absolute inset-y-0 right-0 flex items-center pr-4">
+                <span wire:click="clear" class="cursor-pointer p-1 text-xl text-gray-500"> &times; </span>
             </span>
         </div>
-        @if($properties->count())
-        <div class="absolute mt-1 w-full rounded-md bg-white shadow-lg">
+        <div x-show="open" class="absolute z-10 mt-1 w-full rounded-md bg-white shadow-lg">
             <ul class="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none">
             @foreach($properties as $property)
-                <li class="hover:bg-blue-400 hover:text-white hover:cursor-pointer text-gray-900 select-none relative py-2 pl-3 pr-9">
+                <li wire:click="selectProperty({{ $property->id }})" class="hover:bg-blue-400 hover:text-white hover:cursor-pointer text-gray-900 select-none relative py-2 pl-3 pr-9">
                     {{ $property->nickname }}
                 </li>
             @endforeach
             </ul>
         </div>
-        @endif
     </div>
 </div>
