ab79d88dd4eaf51c2591da7c5574aa9aa3573602
James Wagoner <jameswagoner@gmail.com>
2020-12-12 23:30:25 -0800

    input style

--- resources/views/livewire/properties/new-property.blade.php
+++ resources/views/livewire/properties/new-property.blade.php
@@ -102,9 +102,9 @@
                                 <label for="rent_amount" class="block text-sm font-medium leading-5 text-gray-700">Rent Amount</label>
                                 <div class="mt-1 relative rounded-md shadow-sm">
                                     <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                                        <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                        </svg>
+                                        <span class="text-gray-500 sm:text-sm sm:leading-5">
+                                            $
+                                        </span>
                                     </div>
                                     <input wire:model="rent_amount" id="rent_amount" class="form-input block w-full pl-10 sm:text-sm sm:leading-5">
                                 </div>
