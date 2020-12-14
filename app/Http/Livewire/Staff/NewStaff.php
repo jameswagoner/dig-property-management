95ee7d737be0f2dda3303ec24bd07c7542dc0890
James Wagoner <jameswagoner@gmail.com>
2020-12-13 20:26:50 -0800

    New staff form functions

--- app/Http/Livewire/Staff/NewStaff.php
+++ app/Http/Livewire/Staff/NewStaff.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Staff;
 
+use App\Models\User;
 use Hackzilla\PasswordGenerator\Generator\ComputerPasswordGenerator;
 use Laravel\Fortify\Rules\Password;
 use Livewire\Component;
@@ -11,11 +12,13 @@ class NewStaff extends Component
     public $name;
     public $email;
     public $password;
+    public $group;
 
     protected $rules = [
         'name' => 'required',
         'email' => 'required|email|unique:users',
         'password' => 'required',
+        'group' => 'required'
     ];
 
     public function generatePassword()
@@ -33,6 +36,15 @@ class NewStaff extends Component
     public function save()
     {
         $this->validate();
+
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password)
+        ]);
+
+        $this->dispatchBrowserEvent('toast', 'Staff member was added!');
+        $this->redirectRoute('staff.list');
     }
 
     public function render()
