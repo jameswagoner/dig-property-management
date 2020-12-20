e28be34075adbe6e05dbb38ccc33b5138272f1b1
James Wagoner <jameswagoner@gmail.com>
2020-12-19 17:38:27 -0800

    Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/', function () {
-    return redirect()->route('dashboard');
-});
+require __DIR__.'/auth.php';
