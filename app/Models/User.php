ded9c28e026b047364d88e46d8aef550d945c950
James Wagoner <jameswagoner@gmail.com>
2021-02-15 00:36:46 -0800

    simple permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,23 +7,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Cashier\Billable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Billable,
         HasFactory,
+        HasRoles,
         Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * All attributes are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
---
+++ database/seeders/PermissionsSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit properties']);
+        Permission::create(['name' => 'delete properties']);
+        Permission::create(['name' => 'edit tenants']);
+        Permission::create(['name' => 'delete tenants']);
+
+        // create roles and assign existing permissions
+        $staffRole = Role::create(['name' => 'staff']);
+        $staffRole->givePermissionTo('edit properties');
+        $staffRole->givePermissionTo('delete properties');
+        $staffRole->givePermissionTo('edit tenants');
+        $staffRole->givePermissionTo('delete tenants');
+
+        // create an admin role
+        Role::create(['name' => 'admin']);
+
+        // super admin gets all permissions via Gate::before rule; see AuthServiceProvider
+        Role::create(['name' => 'super-admin']);
+    }
+}
