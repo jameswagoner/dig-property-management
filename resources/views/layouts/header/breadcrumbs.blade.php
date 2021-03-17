0793a16d0a083435bd08ea5ace033990a8d9aaba
James Wagoner <jameswagoner@gmail.com>
2021-03-17 15:22:25 -0700

    breadcrumbs

--- resources/views/layouts/header/breadcrumbs.blade.php
+++ resources/views/layouts/header/breadcrumbs.blade.php
@@ -23,14 +23,7 @@
                             </a>
                         </div>
                     </li>
-                    <li>
-                        <div class="flex items-center">
-                            <svg class="flex-shrink-0 h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                                <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
-                            </svg>
-                            <a href="#" aria-current="page" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Current Page</a>
-                        </div>
-                    </li>
+                    {{ $slot }}
                 </ol>
             </div>
         </nav>
