71d7e9cd714021e9ffa9e43c160aea897f33b73e
James Wagoner <jameswagoner@gmail.com>
2021-04-08 22:36:36 -0700

    forgot how to use invoke I guess

--- app/Http/Controllers/TwilioController.php
+++ app/Http/Controllers/TwilioController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Actions\Twilio\AnswerVoiceAction;
 use Illuminate\Http\Request;
-use Twilio\TwiML\VoiceResponse;
+use Illuminate\Http\Response;
 
 class TwilioController extends Controller
 {
     public function voice(
         Request $request,
         AnswerVoiceAction $answerVoiceAction
-    ): VoiceResponse
+    ): string
     {
-        return $answerVoiceAction;
+        return $answerVoiceAction()->asXML();
     }
 }
