c5aa083747675b6bee37ac2dcebc28de5697caf1
James Wagoner <jameswagoner@gmail.com>
2021-05-12 11:39:14 -0700

    send an sms when we get an sms

--- app/Http/Controllers/Webhooks/Twilio/SmsController.php
+++ app/Http/Controllers/Webhooks/Twilio/SmsController.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Controllers\Webhooks\Twilio;
 
+use App\Actions\NotifyManagementAction;
 use App\Actions\Twilio\StoreSmsAction;
 use App\Http\Controllers\Controller;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Twilio\Rest\Client;
 
 class SmsController extends Controller
 {
     public function incoming(
         Request $request,
-        StoreSmsAction $storeSmsAction
+        StoreSmsAction $storeSmsAction,
+        Client $client
     ): Response
     {
         $storeSmsAction($request, Message::INBOUND);
+
+        (new NotifyManagementAction)($client, "New inbound text message was recieved", ['+13608314766']);
 
         return response('200 OK');
     }
