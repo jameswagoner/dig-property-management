fc0ea7a870dc3e164c051c67b8f2320fda8d1fde
James Wagoner <jameswagoner@gmail.com>
2021-05-31 22:20:34 -0700

    trying to cast as int

--- app/Actions/PayPal/RecordPayment.php
+++ app/Actions/PayPal/RecordPayment.php
@@ -15,8 +15,8 @@ class RecordPayment
             'source' => 'paypal',
             'source_id' => $message->get('txn_id'),
             'status' => $message->get('payment_status'),
-            'amount' => $message->get('mc_gross') * 100,
-            'fee' => $message->get('mc_fee') * 100,
+            'amount' => (int) $message->get('mc_gross') * 100,
+            'fee' => (int) $message->get('mc_fee') * 100,
             'description' => $message->get('memo'),
             'raw' => json_encode($message->getAll()),
         ]);
