c94ae92aebf7dc5ba4d170f4ab14c03a2b789718
James Wagoner <jameswagoner@gmail.com>
2021-07-28 21:00:47 -0700

    adding user id

---
+++ database/migrations/2021_07_28_204439_alter_messages_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->foreignIdFor(User::class, 'user_id')->after('id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            //
+        });
+    }
+};
