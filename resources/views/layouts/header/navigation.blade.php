84476d23f6d09ecb9ad8ec3ee07b93c7495c2adf
James Wagoner <jameswagoner@gmail.com>
2021-09-15 23:44:57 -0700

    tenant menu link

--- resources/views/layouts/header/navigation.blade.php
+++ resources/views/layouts/header/navigation.blade.php
@@ -12,80 +12,30 @@
                  aria-label="Sidebar">
                 <div class="px-2 space-y-1">
 
-                    <a href="#"
-                       class="bg-steel-800 text-white group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md"
-                       x-state:on="Current" x-state:off="Default" aria-current="page"
-                       x-state-description="Current: &quot;bg-steel-800 text-white&quot;, Default: &quot;text-steel-100 hover:text-white hover:bg-steel-600&quot;">
-                        <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200"
-                             x-description="Heroicon name: outline/home" xmlns="http://www.w3.org/2000/svg"
-                             fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+                    <a href="{{ route('manage.dashboard') }}"
+                        @class([
+                            'group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md',
+                            'text-steel-100 hover:text-white hover:bg-steel-600' => Route::currentRouteName() !== 'manage.dashboard',
+                            'bg-steel-800 text-white' => Route::currentRouteName() === 'manage.dashboard'
+                        ])
+                    >
+                        <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200" x-description="Heroicon name: outline/home" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                         </svg>
-                        Home
+                        Dashboard
                     </a>
 
-                    <a href="#"
-                       class="text-steel-100 hover:text-white hover:bg-steel-600 group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md"
-                       x-state-description="undefined: &quot;bg-steel-800 text-white&quot;, undefined: &quot;text-steel-100 hover:text-white hover:bg-steel-600&quot;">
-                        <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200"
-                             x-description="Heroicon name: outline/clock" xmlns="http://www.w3.org/2000/svg"
-                             fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    <a href="{{ route('manage.tenants.index') }}"
+                        @class([
+                            'group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md',
+                            'text-steel-100 hover:text-white hover:bg-steel-600' => Route::currentRouteName() !== 'manage.tenants.index',
+                            'bg-steel-800 text-white' => Route::currentRouteName() === 'manage.tenants.index'
+                        ])
+                    >
+                        <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200" x-description="Heroicon name: outline/user-group" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
                         </svg>
-                        History
-                    </a>
-
-                    <a href="#"
-                       class="text-steel-100 hover:text-white hover:bg-steel-600 group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md"
-                       x-state-description="undefined: &quot;bg-steel-800 text-white&quot;, undefined: &quot;text-steel-100 hover:text-white hover:bg-steel-600&quot;">
-                        <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200"
-                             x-description="Heroicon name: outline/scale" xmlns="http://www.w3.org/2000/svg"
-                             fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"></path>
-                        </svg>
-                        Balances
-                    </a>
-
-                    <a href="#"
-                       class="text-steel-100 hover:text-white hover:bg-steel-600 group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md"
-                       x-state-description="undefined: &quot;bg-steel-800 text-white&quot;, undefined: &quot;text-steel-100 hover:text-white hover:bg-steel-600&quot;">
-                        <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200"
-                             x-description="Heroicon name: outline/credit-card"
-                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"></path>
-                        </svg>
-                        Cards
-                    </a>
-
-                    <a href="#"
-                       class="text-steel-100 hover:text-white hover:bg-steel-600 group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md"
-                       x-state-description="undefined: &quot;bg-steel-800 text-white&quot;, undefined: &quot;text-steel-100 hover:text-white hover:bg-steel-600&quot;">
-                        <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200"
-                             x-description="Heroicon name: outline/user-group"
-                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                        </svg>
-                        Recipients
-                    </a>
-
-                    <a href="#"
-                       class="text-steel-100 hover:text-white hover:bg-steel-600 group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md"
-                       x-state-description="undefined: &quot;bg-steel-800 text-white&quot;, undefined: &quot;text-steel-100 hover:text-white hover:bg-steel-600&quot;">
-                        <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200"
-                             x-description="Heroicon name: outline/document-report"
-                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
-                        </svg>
-                        Reports
+                        Tenants
                     </a>
 
                 </div>
