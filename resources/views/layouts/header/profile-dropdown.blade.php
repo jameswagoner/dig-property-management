252363d2cd6288749cd76e3668c1077173ad92e8
James Wagoner <jameswagoner@gmail.com>
2021-09-15 22:38:56 -0700

    profile dropdown

--- resources/views/layouts/header/profile-dropdown.blade.php
+++ resources/views/layouts/header/profile-dropdown.blade.php
@@ -5,9 +5,9 @@
             x-on:click="onButtonClick()"
             type="button"
             class="max-w-xs bg-white rounded-full flex items-center text-sm focus:outline-none lg:p-2 lg:rounded-md lg:hover:bg-gray-50">
-            <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+            <img class="h-8 w-8 rounded-full" src="https://ui-avatars.com/api/?name={{ auth()->user()->name }}&color=7F9CF5&background=EBF4FF" alt="{{ auth()->user()->name }}">
             <span class="hidden ml-3 text-gray-700 text-sm font-medium lg:block">
-                <span class="sr-only">Open user menu for </span>Emilia Birch</span>
+                <span class="sr-only">Open user menu for </span>{{ auth()->user()->name }}</span>
             <svg class="hidden flex-shrink-0 ml-1 h-5 w-5 text-gray-400 lg:block" x-description="Heroicon name: solid/chevron-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
             </svg>
@@ -18,9 +18,13 @@
         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
             Your Profile
         </a>
-        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-            Logout
-        </a>
+        <form id="logout" class="mb-0" method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <a href="#" onclick="event.preventDefault(); this.closest('form').submit();" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                Logout
+            </a>
+        </form>
     </div>
 
 </div>
