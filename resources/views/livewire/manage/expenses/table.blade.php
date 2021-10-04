36993ddde2cb5e1a9d3eb98d0cb151d689750e95
James Wagoner <jameswagoner@gmail.com>
2021-10-03 22:07:21 -0500

    expenses table tweaks

--- resources/views/livewire/manage/expenses/table.blade.php
+++ resources/views/livewire/manage/expenses/table.blade.php
@@ -1,69 +1,115 @@
-<div class="mt-8">
-    <h2 class="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
-        Tenants
-    </h2>
-
-    <div class="hidden sm:block">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="flex flex-col mt-2">
-                <div class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead>
-                        <tr>
-                            <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Transaction
-                            </th>
-                            <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Amount
-                            </th>
-                            <th scope="col" class="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Date
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                        @forelse($expenses as $expense)
-                            <tr>
-                                <td class="max-w-0 w-full px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                    <div class="flex">
-                                        <a href="#" class="group inline-flex space-x-2 truncate text-sm">
-                                            <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500" x-description="Heroicon name: solid/cash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
-                                            </svg>
-                                            <p class="text-gray-500 truncate group-hover:text-gray-900">
-                                                Payment to Molly Sanders
-                                            </p>
-                                        </a>
-                                    </div>
-                                </td>
-                                <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                                    <span class="text-gray-900 font-medium">$20,000 </span>
-                                    USD
-                                </td>
-                                <td class="hidden px-6 py-4 whitespace-nowrap text-sm text-gray-500 md:block">
-                                    <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800 capitalize">
-                                        success
-                                    </span>
-                                </td>
-                                <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                                    <time datetime="2020-07-11">July 11, 2020</time>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500" colspan="3">
-                                    No expenses exist
-                                </td>
-                            </tr>
-                        @endforelse
-                        </tbody>
-                    </table>
+<div>
+    <x-slot name="pageHeader">
+        <div class="bg-white shadow">
+            <div class="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+                <div class="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
+                    <div class="flex-1 min-w-0">
+                        <div class="flex items-center">
+                            <h1 class="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                                Expenses
+                            </h1>
+                        </div>
+                        <dl class="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
+                            <dt class="sr-only">Address</dt>
+                            <dd class="flex items-center text-sm text-gray-500 font-medium capitalize sm:mr-6">
+                                <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                     x-description="Heroicon name: solid/office-building"
+                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                     fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                          d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z"
+                                          clip-rule="evenodd"></path>
+                                </svg>
+                                Address
+                            </dd>
+                            <dt class="sr-only">Account status</dt>
+                            <dd class="mt-3 flex items-center text-sm text-gray-500 font-medium sm:mr-6 sm:mt-0 capitalize">
+                                <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" x-description="Heroicon name: solid/check-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                </svg>
+                                Good Standing
+                            </dd>
+                        </dl>
+                    </div>
+                    <div class="mt-6 flex space-x-3 md:mt-0 md:ml-4">
+                        <button type="button" class="rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none">
+                            Add
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </x-slot>
 
-    <div>
-        {{ $expenses->links() }}
+    <div class="mt-8">
+        <div class="hidden sm:block">
+            <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex flex-col mt-2">
+                    <div class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead>
+                            <tr>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Transaction
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Unit
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Category
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Amount
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Date
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                            @forelse($expenses as $expense)
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        <div class="flex">
+                                            <a href="#" class="group inline-flex space-x-2 truncate text-sm">
+                                                <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500" x-description="Heroicon name: solid/cash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
+                                                </svg>
+                                                <p class="text-gray-500 truncate group-hover:text-gray-900">
+                                                    {{ $expense->business_name }}
+                                                </p>
+                                            </a>
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        {{ $expense->unit->name }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        {{ $expense->category }}
+                                    </td>
+                                    <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-900 font-medium">
+                                        ${{ $expense->formatted_amount }}
+                                    </td>
+                                    <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
+                                        {{ $expense->expensed_at->toFormattedDateString() }}
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500" colspan="3">
+                                        No expenses exist
+                                    </td>
+                                </tr>
+                            @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            {{ $expenses->links() }}
+        </div>
     </div>
 </div>
