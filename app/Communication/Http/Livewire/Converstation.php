8184fb071d984e80ef6b712b8663e3862fa93a3b
James Wagoner <jameswagoner@gmail.com>
2021-10-09 20:56:00 -0500

    WIP - send message

--- app/Communication/Http/Livewire/Converstation.php
+++ app/Communication/Http/Livewire/Converstation.php
@@ -2,13 +2,18 @@
 
 namespace App\Communication\Http\Livewire;
 
+use App\Actions\Twilio\SendSmsAction;
+use App\DataTransferObjects\MessageData;
+use App\Models\Message;
 use App\Models\User;
+use Exception;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Converstation extends Component
 {
     public $messages;
+    public $message;
     public $user;
 
     public function mount(User $user)
@@ -17,10 +22,27 @@ class Converstation extends Component
         $this->user = $user;
     }
 
+    public function sendMessage(SendSmsAction $sendSmsAction)
+    {
+        try {
+            $messageData = new MessageData([
+                'user_id'   => $this->user->id,
+                'number'    => $this->user->number,
+                'type'      => 'sms',
+                'direction' => Message::OUTBOUND,
+                'body'      => $this->message,
+            ]);
+
+
+        } catch (Exception $e) {
+
+        }
+    }
+
     public function render(): View
     {
         return view('livewire.manage.communication.conversation')
-            ->layout('layouts.base', ['title' => "$this->user->name | Conversation"])
+            ->layout('layouts.base', ['title' => "{$this->user->name} | Conversation"])
             ->with('messages', $this->messages)
             ->with('user', $this->user);
     }
--- resources/views/livewire/manage/communication/conversation.blade.php
+++ resources/views/livewire/manage/communication/conversation.blade.php
@@ -32,14 +32,14 @@
                                     <li>
                                         <div class="flex space-x-3">
                                             <div class="flex-shrink-0">
-                                                <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                                <img class="h-10 w-10 rounded-full" src="https://ui-avatars.com/api/?name={{ $user->name }}&color=7F9CF5&background=EBF4FF" alt="">
                                             </div>
                                             <div>
                                                 <div class="mt-1 text-sm text-gray-700">
                                                     <p>{{ $message->text }}</p>
                                                 </div>
                                                 <div class="mt-2 text-sm space-x-2">
-                                                    <span class="text-gray-500 font-medium">{{ $message->created_at->diffForHumans() }}</span>
+                                                    <span class="text-gray-500 font-light">{{ $message->created_at->diffForHumans() }}</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -50,31 +50,14 @@
                             </div>
                         </div>
                         <div class="bg-gray-50 px-4 py-6 sm:px-6">
-                            <div class="flex space-x-3">
-                                <div class="flex-shrink-0">
-                                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=8&amp;w=256&amp;h=256&amp;q=80" alt="">
+                            <div class="flex items-center justify-between space-x-3">
+                                <div class="form-group flex-1">
+                                    <label for="message-input" class="sr-only">Message</label>
+                                    <input wire:model="message" id="message-input" class="form-control" type="text" placeholder="Enter message" style="margin-top: 0" />
                                 </div>
-                                <div class="min-w-0 flex-1">
-                                    <form action="#">
-                                        <div>
-                                            <label for="comment" class="sr-only">About</label>
-                                            <textarea id="comment" name="comment" rows="3" class="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md" placeholder="Add a note"></textarea>
-                                        </div>
-                                        <div class="mt-3 flex items-center justify-between">
-                                            <a href="#" class="group inline-flex items-start text-sm space-x-2 text-gray-500 hover:text-gray-900">
-                                                <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500" x-description="Heroicon name: solid/question-mark-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
-                                                </svg>
-                                                <span>
-                                                    Some HTML is okay.
-                                                </span>
-                                            </a>
-                                            <button type="submit" class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                                                Comment
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
+                                <button wire:click="sendMessage" type="button" class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                    Send
+                                </button>
                             </div>
                         </div>
                     </div>
