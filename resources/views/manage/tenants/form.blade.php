570a320ca608bb0d2f744f4c1c2bc0884362cf19
James Wagoner <jameswagoner@gmail.com>
2021-10-15 21:23:01 -0500

    view cleanup

--- resources/views/manage/tenants/form.blade.php
+++ resources/views/manage/tenants/form.blade.php
@@ -1,4 +1,4 @@
-<x-base-layout title="{{ $tenant->id ? 'Edit' : 'New' }} Tenant">
+<x-app-layout title="{{ $tenant->id ? 'Edit' : 'New' }} Tenant">
     <h2 class="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
         {{ $tenant->id ? 'Edit' : 'New' }} Tenant
     </h2>
@@ -34,4 +34,4 @@
             </div>
         </div>
     </div>
-</x-base-layout>
+</x-app-layout>
--- resources/views/manage/tenants/index.blade.php
+++ resources/views/manage/tenants/index.blade.php
@@ -1,4 +1,4 @@
-<x-base-layout title="Tenants">
+<x-app-layout title="Tenants">
     <div class="mt-8">
         <h2 class="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
             Tenants
@@ -126,4 +126,4 @@
             </div>
         </div>
     </div>
-</x-base-layout>
+</x-app-layout>
