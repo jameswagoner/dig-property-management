eef4eabd2840ac3726e87582f1040f59f49ae720
James Wagoner <jameswagoner@gmail.com>
2021-10-15 21:01:27 -0500

    fixing mobile menu

--- resources/views/layouts/header/mobile-menu.blade.php
+++ resources/views/layouts/header/mobile-menu.blade.php
@@ -61,23 +61,56 @@
         <nav class="mt-5 flex-shrink-0 h-full divide-y divide-steel-800 overflow-y-auto" aria-label="Sidebar">
             <div class="px-2 space-y-1">
 
-                <a href="#"
-                   class="bg-steel-800 text-white group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                   x-state:on="Current" x-state:off="Default" aria-current="page"
-                   x-state-description="Current: &quot;bg-steel-800 text-white&quot;, Default: &quot;text-steel-100 hover:text-white hover:bg-steel-600&quot;">
+                <a href="{{ route('manage.dashboard') }}"
+                    class="{{ Arr::toCssClasses([
+                        'group flex items-center px-2 py-2 text-base font-medium rounded-md',
+                        'text-steel-100 hover:text-white hover:bg-steel-600' => !Str::contains(Route::currentRouteName(), 'manage.dashboard'),
+                        'bg-steel-800 text-white' => Str::contains(Route::currentRouteName(), 'manage.dashboard')
+                    ]) }}"
+                >
                     <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200" x-description="Heroicon name: outline/home" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                     </svg>
-                    Home
+                    Dashboard
                 </a>
 
-                <a href="#"
-                   class="text-steel-100 hover:text-white hover:bg-steel-600 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                   x-state-description="undefined: &quot;bg-steel-800 text-white&quot;, undefined: &quot;text-steel-100 hover:text-white hover:bg-steel-600&quot;">
-                    <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200" x-description="Heroicon name: outline/clock" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                <a href="{{ route('manage.communication.inbox') }}"
+                    class="{{ Arr::toCssClasses([
+                        'group flex items-center px-2 py-2 text-base font-medium rounded-md',
+                        'text-steel-100 hover:text-white hover:bg-steel-600' => !Str::contains(Route::currentRouteName(), 'manage.communication.'),
+                        'bg-steel-800 text-white' => Str::contains(Route::currentRouteName(), 'manage.communication.')
+                    ]) }}"
+                >
+                    <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200" x-description="Heroicon name: outline/chat-alt-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
                     </svg>
-                    History
+                    Communication
+                </a>
+
+                <a href="{{ route('manage.expenses.index') }}"
+                    class="{{ Arr::toCssClasses([
+                        'group flex items-center px-2 py-2 text-base font-medium rounded-md',
+                        'text-steel-100 hover:text-white hover:bg-steel-600' => !Str::contains(Route::currentRouteName(), 'manage.expenses.'),
+                        'bg-steel-800 text-white' => Str::contains(Route::currentRouteName(), 'manage.expenses.')
+                    ]) }}"
+                >
+                    <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200" x-description="Heroicon name: outline/credit-card" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+                    </svg>
+                    Expenses
+                </a>
+
+                <a href="{{ route('manage.tenants.index') }}"
+                    class="{{ Arr::toCssClasses([
+                        'group flex items-center px-2 py-2 text-base font-medium rounded-md',
+                        'text-steel-100 hover:text-white hover:bg-steel-600' => !Str::contains(Route::currentRouteName(), 'manage.tenants.'),
+                        'bg-steel-800 text-white' => Str::contains(Route::currentRouteName(), 'manage.tenants.')
+                    ]) }}"
+                >
+                    <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200" x-description="Heroicon name: outline/user-group" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                    </svg>
+                    Tenants
                 </a>
 
             </div>
