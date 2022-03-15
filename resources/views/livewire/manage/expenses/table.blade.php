84138680f79582f81fc18469f87f93da9e16b80e
James Wagoner <jameswagoner@gmail.com>
2022-03-14 23:58:40 -0500

    expenses table update

--- resources/views/livewire/manage/expenses/table.blade.php
+++ resources/views/livewire/manage/expenses/table.blade.php
@@ -29,54 +29,45 @@
                             <thead>
                             <tr>
                                 <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Transaction
-                                </th>
-                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Unit
+                                    Date
                                 </th>
                                 <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Category
                                 </th>
-                                <th scope="col" class="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Amount
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Business
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Unit
                                 </th>
                                 <th scope="col" class="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Date
+                                    Amount
                                 </th>
                             </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
                             @forelse($expenses as $expense)
                                 <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        {{ $expense->transacted_at->toFormattedDateString() }}
+                                    </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                        <div class="flex">
-                                            <a href="#" class="group inline-flex space-x-2 truncate text-sm">
-                                                <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500" x-description="Heroicon name: solid/cash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                    <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
-                                                </svg>
-                                                <p class="text-gray-500 truncate group-hover:text-gray-900">
-                                                    {{ $expense->business_name }}
-                                                </p>
-                                            </a>
-                                        </div>
+                                        {{ $expense->category }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        {{ $expense->business_name }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                         {{ optional($expense->unit)->name ?? 'N/A' }}
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        {{ $expense->category }}
-                                    </td>
                                     <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-900 font-medium">
                                         ${{ $expense->formatted_amount }}
-                                    </td>
-                                    <td class="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                                        {{ $expense->expensed_at->toFormattedDateString() }}
                                     </td>
                                 </tr>
                             @empty
                                 <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500" colspan="3">
-                                        No expenses exist
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500" colspan="5">
+                                        No expenses have been recorded
                                     </td>
                                 </tr>
                             @endforelse
