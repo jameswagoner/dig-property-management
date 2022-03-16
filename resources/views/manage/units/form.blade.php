553b07ba9a91eec8d7e4a4013b89180030baef05
James Wagoner <jameswagoner@gmail.com>
2022-03-15 21:52:10 -0500

    setting up units

--- resources/views/manage/units/form.blade.php
+++ resources/views/manage/units/form.blade.php
@@ -1,34 +1,32 @@
-<x-app-layout title="{{ $tenant->id ? 'Edit' : 'New' }} Tenant">
-    <h2 class="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
-        {{ $tenant->id ? 'Edit' : 'New' }} Tenant
-    </h2>
+<x-app-layout title="{{ $unit->id ? 'Edit' : 'New' }} {{ $unit->name ?? 'Unit' }}">
+    <x-slot name="pageHeader">
+        <div class="bg-white shadow">
+            <div class="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+                <div class="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
+                    <div class="flex-1 min-w-0">
+                        <div class="flex items-center">
+                            <div>
+                                <div class="flex items-center">
+                                    <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                                        {{ $unit->id ? 'Edit' : 'New' }} {{ $unit->name ?? 'Unit' }}
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </x-slot>
     <div class="max-w-6xl mx-auto mt-2 px-4 sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow rounded-lg">
             <div class="p-6">
                 <div class="grid grid-cols-1 gap-y-6 sm:grid-cols-6 sm:gap-x-6">
-                    <div class="form-group sm:col-span-3">
+                    <div class="form-group sm:col-span-6">
                         <label for="first-name" >
-                            First name
+                            Name
                         </label>
-                        <input id="first-name" class="form-control" type="text" name="first_name" value="{{ $tenant->first_name }}">
-                    </div>
-                    <div class="form-group sm:col-span-3">
-                        <label for="last-name" >
-                            Last name
-                        </label>
-                        <input id="last-name" class="form-control" type="text" name="last_name" value="{{ $tenant->last_name }}">
-                    </div>
-                    <div class="form-group sm:col-span-3">
-                        <label for="email" >
-                            Email address
-                        </label>
-                        <input id="email" class="form-control" type="text" name="email" value="{{ $tenant->email }}">
-                    </div>
-                    <div class="form-group sm:col-span-3">
-                        <label for="number" >
-                            Phone number
-                        </label>
-                        <input id="number" class="form-control" type="text" name="number" value="{{ $tenant->number }}">
+                        <input id="first-name" class="form-control" type="text" name="name" value="{{ $unit->name }}">
                     </div>
                 </div>
             </div>
