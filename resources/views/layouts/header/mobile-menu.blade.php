2f04455c429d2baa33b7aa43636bc6513e80bbcb
James Wagoner <jameswagoner@gmail.com>
2022-03-23 22:56:26 -0500

    mobile menu

--- resources/views/layouts/header/mobile-menu.blade.php
+++ resources/views/layouts/header/mobile-menu.blade.php
@@ -126,6 +126,19 @@
                     Payments
                 </a>
 
+                <a href="{{ route('manage.units.index') }}"
+                   class="{{ Arr::toCssClasses([
+                            'group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md',
+                            'text-steel-100 hover:text-white hover:bg-steel-600' => !Str::contains(Route::currentRouteName(), 'manage.units.'),
+                            'bg-steel-800 text-white' => Str::contains(Route::currentRouteName(), 'manage.units.')
+                        ]) }}"
+                >
+                    <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
+                    </svg>
+                    Units
+                </a>
+
                 <a href="{{ route('manage.tenants.index') }}"
                     class="{{ Arr::toCssClasses([
                         'group flex items-center px-2 py-2 text-base font-medium rounded-md',
