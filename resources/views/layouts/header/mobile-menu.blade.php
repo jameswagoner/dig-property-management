d26fdef24213cb4e50cb9454235e6d939439ee96
James Wagoner <jameswagoner@gmail.com>
2023-01-14 22:03:08 -0600

    remove expenses links

--- resources/views/layouts/header/mobile-menu.blade.php
+++ resources/views/layouts/header/mobile-menu.blade.php
@@ -87,19 +87,6 @@
                     Communication
                 </a>
 
-                <a href="{{ route('manage.expenses.index') }}"
-                   class="{{ Arr::toCssClasses([
-                            'group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md',
-                            'text-steel-100 hover:text-white hover:bg-steel-600' => !Str::contains(Route::currentRouteName(), 'manage.expenses.'),
-                            'bg-steel-800 text-white' => Str::contains(Route::currentRouteName(), 'manage.expenses.')
-                        ]) }}"
-                >
-                    <svg class="mr-4 flex-shrink-0 h-6 w-6 text-steel-200" x-description="Heroicon name: outline/credit-card" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-                    </svg>
-                    Expenses
-                </a>
-
                 <a href="{{ route('manage.labor.index') }}"
                    class="{{ Arr::toCssClasses([
                             'group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md',
